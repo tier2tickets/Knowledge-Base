1b612b3dac51197e38948a9db12e87ed2cbd5101
Chris Wheeler <grintor@gmail.com>
2019-08-05 09:44:13 -0400

    no caching!

--- _api/pull_kb.sh
+++ _api/pull_kb.sh
@@ -6,4 +6,11 @@ git pull
 rm -r _build
 sphinx-build -M dirhtml . _build > /dev/null 2>&1
 sphinx-build -M dirhtml . _build
+
 echo ErrorDocument 404 https://docs.tier2tickets.com > _build/dirhtml/.htaccess
+
+echo \<IfModule mod_headers.c\> >> _build/dirhtml/.htaccess
+echo 	Header set Cache-Control "no-cache, no-store, must-revalidate" >> _build/dirhtml/.htaccess
+echo 	Header set Pragma "no-cache" >> _build/dirhtml/.htaccess
+echo 	Header set Expires 0 >> _build/dirhtml/.htaccess
+echo \</IfModule\> >> _build/dirhtml/.htaccess
